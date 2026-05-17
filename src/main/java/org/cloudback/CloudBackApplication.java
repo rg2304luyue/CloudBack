26f421e8c9c5635df0402db0f697afb8be881b28
mineheartx <2779902707@qq.com>
2026-05-17 10:44:15 +0800

    first commit

---
+++ src/main/java/org/cloudback/CloudBackApplication.java
@@ -0,0 +1,13 @@
+package org.cloudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBackApplication.class, args);
+	}
+
+}
